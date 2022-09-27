41cb752d4ec31641a606753f1ccd81d8fd2c5f1c
Mahesh <mahesh_para@havard.edu>
2022-09-27 21:30:34 +0530

    tes

--- test.sql
+++ test.sql
@@ -1,4 +1,10 @@
+declare
+    l_workspace_id number;
 BEGIN
+   select workspace_id into l_workspace_id
+      from apex_workspaces
+    where workspace = 'TEST';
+    apex_application_install.set_workspace_id(l_workspace_id);
     APEX_INSTANCE_ADMIN.REMOVE_APPLICATION(100001);
 END;
 /
